b626000d9475413d9922cfe0422f422114109d3a
Brian McAllister <brian@brianmcallister.com>
2011-04-30 18:48:51 -0400

    added favicon reference

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 	<meta name="author" content="Brian McAllister">
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 	
+  	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="author" href="humans.txt" />
 	<link rel="stylesheet" href="/css/style.css">
 	<link rel="stylesheet" media="handheld" href="/css/handheld.css">

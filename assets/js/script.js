6fc4e64be1d4c65d4cba0f5f589c33698bc25ece
Brian McAllister <brian@brianmcallister.com>
2011-09-22 16:56:36 -0400

    Set up parallax scrolling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,232 @@
-/* Author: Brian McAllister
+/*
+
+    Author: Brian McAllister
 
 */
-
 $( function() {
   
-  window.projects = {
-    
-    project: {
-      
-      'title': 'Project 1',
-      'link': 'http://example.com',
-      'type': ['dev', 'design'],
-      'images': ['image1.jpg', 'image2.jpg']
-      
-    },
-    
-    project: {
-      
-      'title': 'Project 2',
-      'link': 'http://example.com',
-      'type': ['dev'],
-      'images': ['image3.jpg', 'image4.jpg']
-      
-    },
-    
-    project: {
-      
-      'title': 'Project 3',
-      'link': 'http://example.com',
-      'type': ['design'],
-      'images': ['image5.jpg', 'image6.jpg']
-      
-    }
-    
-  }; // projects
+  var random = function(min, max) {
+    var adjustedMax = (parseFloat(max) - parseFloat(min)) + 1;
+    return Math.floor( Math.random() * adjustedMax ) + parseFloat( min );
+  };
   
+  var windowHeight  = $(window).height(),
+      $body         = $('body'),
+      
+      // Four parallax effect blocks
+      $blocks       = $('.blocks'),
+      $block1       = $('#block0'),
+      $block2       = $('#block1'),
+      $block3       = $('#block2'),
+      $block4       = $('#block3'),
+      blockOffset   = [],
+      blockFactor   = [],
+      move;
+      
 
-  window.Project = Backbone.Model.extend({
-
+  $(window).load( function() {
     
-  });
-  
-  window.ProjectList = Backbone.Collection.exten({
+    var docHeight     = $(document).height(),
+        blockRange    = parseInt(docHeight / 4, 10),
+        currentRange  = 0,
+        updateTop;
     
-    model: Project
-    
-  });
-  
-  window.Projects = new ProjectList;
-  
-  window.ProjectView = Backbone.View.extend({
-    
-    tag: 'article',
-    
-    template: _.template( $('#project-template').html() )
-  
-  });
-  
-  window.PortfolioView = Backbone.View.extend({
-    
-    el: $('#contain'),
-    
-    init: function() {
+    $blocks.each( function( index, el ) {
       
-      Projects.fetch();
-    }
+      var top     = random(currentRange, currentRange + blockRange),
+          height  = random(1000, docHeight),
+          color   = '0.0' + random(10, 50).toPrecision(2),
+          factor  = random(5, 20);
+
+      $(el).css({
+        'top'     : top,
+        'height'  : height,
+        'background-color'  : 'rgba(0, 0, 0, ' + color + ')'
+      }).data('factor', factor).fadeIn();
+      
+      currentRange += blockRange;
+
+    }); // each
+    
+    $(this).scroll( function() {
+      
+      $blocks.each( function( index, el ) {
+        
+        if ( window.pageYOffset < move ) {
+          updateTop = blockOffset[index] + $(el).data('factor');
+        } else {
+          updateTop = blockOffset[index] - $(el).data('factor');
+        }
+        
+        $(el).offset({ top: updateTop, left: 0 });
+        blockOffset[index] = $(el).offset().top;
+        
+        console.log($(el).data('factor'))
+      });
+      
+      move = this.pageYOffset;
+      
+    });
+    
     
   });
+    
+  // $(window).scroll( function() {
+  //   
+  //   $blocks.each( function( index, el ) {
+  //     
+  //     if ( window.pageYOffset < move ) {
+  //       top = blockOffset[index] + $(el).data('factor');
+  //     } else {
+  //       top = blockOffset[index] - $(el).data('factor');
+  //     }
+  //     
+  //     $(el).offset({ top: top, left: 0 })
+  //     blockOffset[index] = $(el).offset().top;
+  //     
+  //   });
+  //   
+  //   move = this.pageYOffset;
+  //   
+  // });
   
-  window.Portfolio = new PortfolioView;
   
   
+      
+  // 
+  // 
+  // 
+  // 
+  // 
+  // 
+  //     
+  //     
+  //     
+  //     
+  // 
+  // // Only set up the blocks once the page is fully loaded.    
+  // $(window).load( function() {
+  //   
+  //   $body.height( $(document).height() );
+  //   
+  //   var randPos         = random(1, windowHeight),
+  //       randHeight      = random(500, 1000),
+  //       randColor       = '0.0' + random(50, 80).toPrecision(2),
+  //       randFactor      = random(15, 25),
+  //       
+  //       blockTemplate   = $('#block-template').html(),
+  //       // $block          = $(blockTemplate).appendTo( $body ),
+  //       
+  //       $project        = $('article').first(),
+  //       $projectPos     = $project.offset(),
+  //       
+  //       currentOffset, previousOffset, top;
+  //   
+  //   // $block.css({
+  //   //   'top'               : randPos,
+  //   //   'height'            : randHeight,
+  //   //   'background-color'  : 'rgba(0, 0, 0, ' + randColor + ')'
+  //   // }).fadeIn();
+  //   
+  //   for ( var i = 0; i < 3; i++ ) {
+  //     
+  //     var name    = 'block' + i,
+  //         pos     = random(1, windowHeight),
+  //         height  = random(500, 1000),
+  //         color   = '0.0' + random(50, 80).toPrecision(2),
+  //         factor  = random(15, 25);
+  //         
+  //     $body.append( 
+  //       
+  //       $(blockTemplate).addClass(name).css({
+  //         'top'     : pos,
+  //         'height'  : height,
+  //         'background-color'  : 'rgba(0, 0, 0, ' + color + ')'
+  //       });
+  //     
+  //     );
+  //     
+  //     
+  //   }
+  //     
+  //     
+  //     
+  //   });
+  //   
+  //   
+  //   $(window).scroll( function( event ) {
+  // 
+  //     currentOffset = $block.offset();
+  //     
+  //     this.pageYOffset > previousOffset ? top = currentOffset.top - randFactor : top = currentOffset.top + randFactor;
+  //   
+  //     $block.offset({'top' : top, 'left' : 0});
+  // 
+  //     
+  //     // if ( $projectPos.top < previousOffset ) {
+  //     //   console.log('adfasfasdf');
+  //     // }
+  // 
+  //     previousOffset = this.pageYOffset;
+  // 
+  //   });
+  //   
+  // });
   
+ 
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 
+// $( function() {
+//   
+//   window.portfolioItems = [
+//     
+//     {  
+//       'title': 'Project 1',
+//       'description': 'blah blha blah',
+//       'link': 'http://example.com',
+//       'type': ['dev', 'design'],
+//       'images': ['image1.jpg', 'image2.jpg'] 
+//     },
+//     
+//     {  
+//       'title': 'Project 2',
+//       'description': 'blah blha blah',
+//       'link': 'http://example.com',
+//       'type': ['dev'],
+//       'images': ['image1.jpg', 'image2.jpg'] 
+//     },
+//     
+//     {  
+//       'title': 'Project 3',
+//       'description': 'blah blha blah',
+//       'link': 'http://example.com',
+//       'type': ['design'],
+//       'images': ['image1.jpg', 'image2.jpg'] 
+//     }
+//     
+//   ];
+//   
+//   window.Project = Backbone.Model.extend({});
+//   
+//   _.each( portfolioItems, function(project) {
+//     console.log(project);
+//     window.projects = new Project(project);
+//   });
+//   
+//   
+//   
+//   // window.project = new Project;
+//   
+//   // window.project.set({
+//   //   title: 'Project 1',
+//   //   description: 'blabhablhba balhba',
+//   //   link: 'http://example.com',
+//   //   type: ['dev', 'design'],
+//   //   images: ['image1.jpg', 'image2.jpg']
+//   // });
+//     
+// });

b0acc953d5e25e382be970e66f04232a90ca9415
Brian McAllister <brian@brianmcallister.com>
2020-10-04 13:14:30 -0400

    Add newest content.

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -35,6 +35,10 @@ export const CONTENT: ContentBlock[] = [
         href: 'https://youtu.be/oCY90j3xCKI',
         title: 'Creating, Maintaining and Using a Design System',
       },
+      {
+        href: 'https://youtu.be/5_gkDf4YDJ8',
+        title: 'Creating Icons for Digital Platforms',
+      },
     ],
   },
   {
@@ -64,10 +68,17 @@ export const CONTENT: ContentBlock[] = [
         href: 'https://github.com/brianmcallister/use-api-poller-worker',
         title: 'use-api-poller-worker',
       },
-
       {
         href: 'https://github.com/brianmcallister/sequential-promise',
         title: 'sequential-promise',
+      },
+      {
+        href: 'https://github.com/lab49/react-order-book',
+        title: '@lab49/react-order-book',
+      },
+      {
+        href: 'https://github.com/lab49/react-value-flash',
+        title: '@lab49/react-value-flash',
       },
     ],
   },
@@ -85,6 +96,15 @@ export const CONTENT: ContentBlock[] = [
       {
         href: 'https://www.linkedin.com/in/brian-wm-mcallister/',
         title: 'LinkedIn',
+      },
+    ],
+  },
+  {
+    title: 'Currently',
+    items: [
+      {
+        href: 'https://www.lab49.com/',
+        title: 'Engineering Practice Lead at Lab49',
       },
     ],
   },
